dd127b76cd51fca27cdfa6ebe3893e335961077d
alphasec <95212748+alphasecio@users.noreply.github.com>
2025-05-05 14:28:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,39 +4,98 @@ from google.cloud import modelarmor_v1
 
 # Google Cloud & Gemini settings
 gcp_project_id = os.getenv("PROJECT_ID")
-gcp_location = os.getenv("LOCATION", "us-central1")  # Default to 'us-central1' if not set
+gcp_location = os.getenv("LOCATION", "us-central1")
+gcp_ma_endpoint = os.getenv("MA_ENDPOINT", "modelarmor.us-central1.rep.googleapis.com")
+
 model_options = {
     "Gemini 1.5 Flash": "gemini-1.5-flash",
     "Gemini 2.0 Flash": "gemini-2.0-flash"
 }
 
-# Create the following Model Armor templates beforehand
-template_options = {
-    "None": "none",
-    "All - high only": "ma-all-high",
-    "All - medium and above": "ma-all-med",
-    "All - low and above": "ma-all-low",
-    "Prompt injection and jailbreak - high only": "ma-pijb-high",
-    "Prompt injection and jailbreak - medium and above": "ma-pijb-med",
-    "Prompt injection and jailbreak - low and above": "ma-pijb-low",
-    "Sensitive data protection - basic only": "ma-sdp-basic",
-    "Malicious URL detection - only": "ma-mal-url",
-    "Responsible AI - high only": "ma-rai-high",
-    "Responsible AI - medium and above": "ma-rai-med",
-    "Responsible AI - low and above": "ma-rai-low"
-}
+# Ensure the following Model Armor templates are available in the specified Google Cloud project
+# "None": "none"
+# "All - high only": "ma-all-high"
+# "All - medium and above": "ma-all-med"
+# "All - low and above": "ma-all-low"
+# "Prompt injection and jailbreak - high only": "ma-pijb-high"
+# "Prompt injection and jailbreak - medium and above": "ma-pijb-med"
+# "Prompt injection and jailbreak - low and above": "ma-pijb-low"
+# "Sensitive data protection - basic only": "ma-sdp-basic"
+# "Malicious URL detection - only": "ma-mal-url"
+# "Responsible AI - high only": "ma-rai-high"
+# "Responsible AI - medium and above": "ma-rai-med"
+# "Responsible AI - low and above": "ma-rai-low"
 
 # Streamlit app config
 st.set_page_config(page_title="Model Armor Demo", page_icon="🛡️", initial_sidebar_state="auto")
 
 # Model Armor settings
 with st.sidebar:
-    st.title("🛡️ Model Armor Demo")
-    creds_file = st.file_uploader("Google Cloud credentials file", type="json")
-    model_option = st.selectbox("Model", list(model_options.keys()))
-    model = model_options[model_option]
-    sanitize_request = st.selectbox("Sanitize prompt request?", list(template_options.keys()))
-    sanitize_response = st.selectbox("Sanitize model response?", list(template_options.keys()))
+    st.header("🛡️ Model Armor Demo")
+    with st.expander("**⚙️ Model Settings**", expanded=True):
+        model_option = st.selectbox("Model", list(model_options.keys()))
+        model = model_options[model_option]    
+        creds_file = st.file_uploader("Google Cloud credentials file", type="json") 
+    
+    with st.expander("**⚙️ Model Armor Settings**", expanded=True):
+        detection_type = None
+        confidence_level = None
+        sanitize_request = st.checkbox("Sanitize prompt request?")
+        
+        if sanitize_request:
+            detection_type = st.radio(
+                "**Detection type**",
+                [
+                    "Malicious URLs",
+                    "Sensitive data protection",
+                    "Prompt injection and jailbreak",
+                    "Responsible AI",
+                    "All of the above"
+                ]
+            )
+
+            if detection_type in [
+                "Prompt injection and jailbreak",
+                "Responsible AI",
+                "All of the above"
+            ]:
+                confidence_level = st.radio(
+                    "**Confidence level**",
+                    ["High only", "Medium and above", "Low and above"]
+                )
+
+        # Map detection_type and confidence_level to templates
+        if detection_type == "Malicious URLs":
+            template_id = "ma-mal-url"
+        elif detection_type == "Sensitive data protection":
+            template_id = "ma-sdp-basic"
+        elif detection_type == "Prompt injection and jailbreak":
+            if confidence_level == "High only":
+                template_id = "ma-pijb-high"
+            elif confidence_level == "Medium and above":
+                template_id = "ma-pijb-med"
+            elif confidence_level == "Low and above":
+                template_id = "ma-pijb-low"
+        elif detection_type == "Responsible AI":
+            if confidence_level == "High only":
+                template_id = "ma-rai-high"
+            elif confidence_level == "Medium and above":
+                template_id = "ma-rai-med"
+            elif confidence_level == "Low and above":
+                template_id = "ma-rai-low"
+        elif detection_type == "All of the above":
+            if confidence_level == "High only":
+                template_id = "ma-all-high"
+            elif confidence_level == "Medium and above":
+                template_id = "ma-all-med"
+            elif confidence_level == "Low and above":
+                template_id = "ma-all-low"
+
+        sanitize_response = st.checkbox("Sanitize model response?", help="Uses `All - low and above` template")
+
+    with st.expander("**⚙️ Llama Guard Settings**", expanded=False):
+        include_llama_guard = st.checkbox("Include Llama Guard?", disabled=True)
+        # groq_api_key = st.text_input("Groq API key", type="password", help="Get your API key [here](https://console.groq.com/keys).")
 
 # Cache credentials to avoid unnecessary writes
 if creds_file is not None and "google_creds" not in st.session_state:
@@ -55,16 +114,12 @@ if creds_file is not None:
     if "model_armor_client" not in st.session_state:
         st.session_state.model_armor_client = modelarmor_v1.ModelArmorClient(
             transport="rest",
-            client_options={"api_endpoint": "modelarmor.us-central1.rep.googleapis.com"},
+            client_options={"api_endpoint": gcp_ma_endpoint},
         )
 
 # Initialise session state for model and messages
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
 # Print readable match state message
 def get_match_state_message(match_state):
@@ -105,6 +160,10 @@ def print_results(response):
     st.write(f"* **Harassment**: {get_match_state_message(rai_harassment_match_state)}")
     st.write(f"* **Dangerous**: {get_match_state_message(rai_dangerous_match_state)}")
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 # User-Assistant chat interaction
 if creds_file is not None and st.session_state.vertex_client:
     if prompt := st.chat_input("Ask anything"):
@@ -112,11 +171,9 @@ if creds_file is not None and st.session_state.vertex_client:
             st.markdown(prompt)
             st.session_state.messages.append({"role": "user", "content": prompt})
             
-        if sanitize_request != "None":
+        if sanitize_request:
             try:
-                template_id = template_options[sanitize_request]
-                prompt_data = modelarmor_v1.DataItem()
-                prompt_data.text = prompt
+                prompt_data = modelarmor_v1.DataItem(text=prompt)
                 request = modelarmor_v1.SanitizeUserPromptRequest(
                     name=f"projects/{gcp_project_id}/locations/{gcp_location}/templates/{template_id}",
                     user_prompt_data=prompt_data,
@@ -126,7 +183,6 @@ if creds_file is not None and st.session_state.vertex_client:
                 if response.sanitization_result.filter_match_state == 2:
                     with st.container(border=True):
                         print_results(response)
-                    
                     with st.expander("Sanitised prompt request (raw)", expanded=False):
                         with st.container(height=300, border=True):
                             st.write(response)
@@ -143,12 +199,11 @@ if creds_file is not None and st.session_state.vertex_client:
             except Exception as e:
                 st.error(f"Vertex AI error: {e}")
 
-        if sanitize_response != "None":
+        if sanitize_response:
             with st.spinner("Analysing model response..."):
                 try:
-                    template_id = template_options[sanitize_response]
-                    model_data = modelarmor_v1.DataItem()
-                    model_data.text = model_response.text
+                    template_id = "ma-all-low"
+                    model_data = modelarmor_v1.DataItem(text=model_response.text)
                     request = modelarmor_v1.SanitizeModelResponseRequest(
                         name=f"projects/{gcp_project_id}/locations/{gcp_location}/templates/{template_id}",
                         model_response_data=model_data,
@@ -158,7 +213,6 @@ if creds_file is not None and st.session_state.vertex_client:
                     if response.sanitization_result.filter_match_state == 2:
                         with st.container(border=True):
                             print_results(response)
-                        
                         with st.expander("Sanitised model response (raw)", expanded=False):
                             with st.container(height=300, border=True):
                                 st.write(response)
